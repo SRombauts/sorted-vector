904817c242c34fcb1c279facc3d647670e90dd6d
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-10-14 19:50:39 +0200

    Fixed MSVC build
     - merged from my CppSkeleton project

--- src/utils/time.cpp
+++ src/utils/time.cpp
@@ -11,11 +11,17 @@
 
 #include "utils/time.h"
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 namespace Utils {
 
-// Get tick in microseconds.
+// Get tick in milliseconds
 time_t Time::getTickMs() {
     time_t          TickMs = 0;
+#if defined(__GNUC__)
     int             Res;
     struct timespec ts_now;
 
@@ -24,13 +30,22 @@ time_t Time::getTickMs() {
     if (0 == Res) {
         TickMs = (ts_now.tv_sec * 1000) + (ts_now.tv_nsec / 1000000);
     }
+#elif defined(_WIN32)
+    LARGE_INTEGER freq;
+    if (FALSE != QueryPerformanceFrequency(&freq)) {
+        LARGE_INTEGER t1;
+        QueryPerformanceCounter(&t1);
+        TickMs = (time_t) ((t1.QuadPart) / (freq.QuadPart / 1000));
+    }
+#endif
 
     return TickMs;
 }
 
-// Get tick in milliseconds.
+// Get tick in microseconds
 time_t Time::getTickUs() {
     time_t          TickUs = 0;
+#if defined(__GNUC__)
     int             Res;
     struct timespec ts_now;
 
@@ -39,11 +54,20 @@ time_t Time::getTickUs() {
     if (0 == Res) {
         TickUs = (ts_now.tv_sec * 1000000) + (ts_now.tv_nsec / 1000);
     }
+#elif defined(_WIN32)
+    LARGE_INTEGER freq;
+    // TODO(SRombauts) could be done only once ?
+    if (FALSE != QueryPerformanceFrequency(&freq)) {
+        LARGE_INTEGER t1;
+        QueryPerformanceCounter(&t1);
+        TickUs = (time_t) ((t1.QuadPart) / (freq.QuadPart / 1000000));
+    }
+#endif  // __GNUC__
 
     return TickUs;
 }
 
-// Calculate difference between consecutive ticks.
+// Calculate difference between consecutive ticks
 time_t Time::diff(const time_t aStartTime, const time_t aEndTime) {
     time_t deltaTime;
     if (aStartTime <= aEndTime) {
